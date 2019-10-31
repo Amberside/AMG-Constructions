3a21451d3b5b1117a94376e50f220f266d5956d7
matthew redman <matt.redman@outlook.com>
2019-11-01 09:29:30 +1100

    updates

--- site/js/copyright.js
+++
@@ -1,4 +0,0 @@
-function getyear() {
-    var year = Date.now().getyear
-    document.getElementById(getyear).innerHTML = year
-}
--- site/js/projects/gall.js
+++ site/js/projects/gall.js
@@ -1,3 +1,20 @@
+document.onkeydown = function (event) {
+    switch (event.keyCode) {
+        case 37:
+            // alert('Left key pressed');
+            plusSlides(-1)
+            break;
+        case 39:
+            plusSlides(1)
+            break;
+        case 27:
+            closeModal()
+
+            break;
+    }
+};
+
+
 function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
@@ -20,17 +37,19 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    // var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
+
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].class = dots[i].class.replace(" active", "");
+    // }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+    // dots[slideIndex - 1].className += " active";
+
+    // captionText.innerHTML = dots[slideIndex - 1].alt;
 }
